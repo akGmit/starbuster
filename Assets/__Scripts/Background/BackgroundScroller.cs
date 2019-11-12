4337a28dc3b4f106d7781bb0e4f03bda795ff56c
akgmit <andrius.korsakas@gmail.com>
2019-11-12 12:53:10 +0000

    level 1: scrolling background, player movement

---
+++ Assets/__Scripts/Background/BackgroundScroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * use a quad to set up a scrolling background
+ * see unity documentation https://docs.unity3d.com/Manual/PrimitiveObjects.html?_ga=2.18934193.804407618.1572951697-41484169.1568376042
+ * 
+ * to scroll,
+ * need scroll speed, material, quad
+ */
+
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 1.0f;
+    private Vector2 offset; // use in update
+    private Material myMaterial;
+
+    void Start()
+    {
+        // get the material and set the offset value
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0, scrollSpeed); 
+    }
+
+    void Update()
+    {
+        // set the x offset
+        myMaterial.mainTextureOffset += 
+                            offset * Time.deltaTime;
+    }
+}
---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // == fields ==
+    [SerializeField]    // adds this field to the Unity editor
+    private float playerSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()    // initialisation
+    {
+    }
+
+    // == private methods ==
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // need to ensure that deltaX is frame rate independent
+        // use Time.deltaTime as a multiplier
+        // also add a speed multiplier for the player to get a good feel
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        
+        // add the delta to the current position
+        var newXPos = transform.position.x + deltaX;
+        
+        // update the current position
+        transform.position = new Vector2(newXPos, transform.position.y);
+    }
+
+
+}
